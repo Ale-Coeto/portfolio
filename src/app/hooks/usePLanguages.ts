d11e3a3376e3f3f8df4489388a8cfe93971378c0
Alejandra Coeto <a01285221@tec.mx>
2025-09-10 23:20:52 -0700

    added dart lang

--- src/app/hooks/usePLanguages.ts
+++ src/app/hooks/usePLanguages.ts
@@ -1,51 +1,56 @@
-
 const usePLanguages = () => {
-    const pLanguages = [
-        {
-            icon: '/images/icons/Java.png',
-            name: 'Java',
-            time: '5 years',
-            level: 80
-        },
-        {
-            icon: '/images/icons/C++.png',
-            name: 'C++',
-            time: '4 years',
-            level: 60
-        },
-        {
-            icon: '/images/icons/Python.png',
-            name: 'Python',
-            time: '4 years',
-            level: 50
-        },
-        {
-            icon: '/images/icons/Typescript.png',
-            name: 'Typescript',
-            time: '3 years',
-            level: 50
-        },
-        {
-            icon: '/images/icons/Javascript.png',
-            name: 'JavaScript',
-            time: '1 year',
-            level: 50
-        },
-        {
-            icon: '/images/icons/Swift.png',
-            name: 'Swift',
-            time: '2 years',
-            level: 80
-        },
-        {
-            icon: '/images/icons/Matlab.png',
-            name: 'Matlab',
-            time: '< 1 year',
-            level: 80
-        },
-    ]
+  const pLanguages = [
+    {
+      icon: "/images/icons/Java.png",
+      name: "Java",
+      time: "5 years",
+      level: 80,
+    },
+    {
+      icon: "/images/icons/C++.png",
+      name: "C++",
+      time: "4 years",
+      level: 60,
+    },
+    {
+      icon: "/images/icons/Python.png",
+      name: "Python",
+      time: "4 years",
+      level: 50,
+    },
+    {
+      icon: "/images/icons/Typescript.png",
+      name: "Typescript",
+      time: "3 years",
+      level: 50,
+    },
+    {
+      icon: "/images/icons/Javascript.png",
+      name: "JavaScript",
+      time: "1 year",
+      level: 50,
+    },
+    {
+      icon: "/images/icons/Swift.png",
+      name: "Swift",
+      time: "2 years",
+      level: 80,
+    },
+    {
+      icon: "/images/icons/Matlab.png",
+      name: "Matlab",
+      time: "< 1 year",
+      level: 80,
+    },
+    {
+      icon: "/images/icons/Dart.png",
+      name: "Dart",
+      time: "< 1 year",
+      level: 60,
+    },
+  ];
 
-    return pLanguages;
-}
+  return pLanguages;
+};
 
 export default usePLanguages;
